7b5334409e8fd0942744773ff50b1c566c7132b4
Kaiyuan Liu <jungle.chuan@gmail.com>
2015-03-09 22:58:14 +0000

    changes before refactoring

--- functional_test.py
+++ functional_test.py
@@ -24,16 +24,19 @@ class NewVistorTest(unittest.TestCase):
         self.assertEqual(
                 input_box.get_attribute('placeholder'),
                 'Enter a to-do item')
+
+        input_box.send_keys('Use peacock feathers to make a fly')
         input_box.send_keys(Keys.ENTER)
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in the table"
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly',
+            [row.text for row in rows]
         )
-        self.fail("finish the test")
 
+        self.fail("finish the test")
 
 if __name__ == "__main__":
 
